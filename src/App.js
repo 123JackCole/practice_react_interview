bc424f94e15803d21654c13a6c6be16101a8d93a
123JackCole <123jack.cole@gmail.com>
2021-04-16 17:02:37 -0700

    added a button that increments a counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const { useState } = React;
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  const handleClick = () => {
+    counter++;
+  };
+
+  const endpoint = "https://randomuser.me/api";
+  const [APIFetch, setAPIFetch] = useState();
+
+  const fetchAPI = (endpoint) => {
+    fetch(endpoint)
+      .then((res) => res.json())
+      .then((data) => setAPIFetch(data));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h1>{counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        +1
+      </button>
+      <br></br>
+      <br></br>
+      <br></br>
+      {fetchAPI()}
+      <h2>{APIFetch}</h2>
     </div>
   );
 }
